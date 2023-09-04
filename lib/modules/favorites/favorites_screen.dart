9430fdabbde1cb47fb0bd50e54f3490c47511ec7
alymu <alym7mdd@gmail.com>
2023-09-05 01:27:00 +0300

    initial commit

--- lib/modules/favorites/favorites_screen.dart
+++ lib/modules/favorites/favorites_screen.dart
@@ -31,11 +31,16 @@ class _FavoritesScreenState extends State<FavoritesScreen>
 
   List <String> images =
   [
-    'https://img.freepik.com/free-photo/vertical-shot-tree-with-dark-cloud_181624-3109.jpg?w=740&t=st=1692651747~exp=1692652347~hmac=422df3740ecbfb028f6f6f944d03b172fa055f8c875f4b7a5e571aba01cac154',
+    'https://img.freepik.com/free-photo/3d-grunge-room-interior-with-spotlight-smoky-atmosphere-background_1048-11333.jpg?w=996&t=st=1693865943~exp=1693866543~hmac=11af7c419f3565d69f4795eac6f4f8f21b07e83ebf980e75c90c0f34a379c52a',
+    'https://img.freepik.com/free-photo/old-painted-textured-surface-backdrop_114579-40052.jpg?w=1060&t=st=1693866018~exp=1693866618~hmac=19bd4859a9187d580032229dc958ad8a85e6fdcea5d590f47850632a2a1ec857',
     'https://img.freepik.com/free-photo/tree-with-starry-night-sky_23-2148282334.jpg?w=360&t=st=1692654061~exp=1692654661~hmac=11864a374fb234719576f4722b7d70da6483b02982356871975cda38cb4004eb',
-    'https://img.freepik.com/free-photo/breathtaking-shot-sea-dark-purple-sky-filled-with-stars_181624-23013.jpg?w=1060&t=st=1692654113~exp=1692654713~hmac=37b07474b1fe6cd69fe166e920ae88b3155d18d08887d57f1b3840c20903f356',
     'https://img.freepik.com/free-photo/beautiful-picture-cloudy-sunset-cliffs-sea-malta_181624-18354.jpg?t=st=1692651747~exp=1692652347~hmac=fc40f78f6d92213cbeab32258c3f7a4650d1a4dc0593a6812535a9d6cdf81428',
     'https://img.freepik.com/free-photo/low-angle-shot-mesmerizing-starry-sky_181624-27925.jpg?t=st=1692620249~exp=1692620849~hmac=b9b600973538c9d89af558ff55abcc040461e85a4a26869590b6ef912bd904a2',
+    'https://img.freepik.com/free-photo/vertical-shot-breathtaking-starry-sky-night-perfect-wallpapers-backgrounds_181624-25954.jpg?w=360&t=st=1693866051~exp=1693866651~hmac=8a23214f372788bba753261791b3773732fdda261f5508c8737a4d143ea2a376',
+    'https://img.freepik.com/free-photo/breathtaking-shot-starry-night-bolonia-beach-algeciras-cadiz-spain_181624-22684.jpg?w=1060&t=st=1693866074~exp=1693866674~hmac=cfd8976df1b9720d313eb37f7aa215cc5325e4d5193093610dede358984ef2d5',
+    'https://img.freepik.com/free-photo/dark-blue-product-background_53876-92801.jpg?w=1060&t=st=1693866126~exp=1693866726~hmac=685aef4a95d8aacb684cfe8be736d78e42b860044c79d8ace96592897da0eb09',
+    'https://img.freepik.com/free-photo/3d-render-spotlights-grunge-brick-wall_1048-6284.jpg?w=740&t=st=1693866185~exp=1693866785~hmac=c53a8af84dc86a1fcc7a3f7e08d8b31c1468cf749d6a64f4e762224e65922834',
+    'https://img.freepik.com/free-photo/metal-grunge-background-with-scratches-stains_1048-14029.jpg?w=740&t=st=1693866343~exp=1693866943~hmac=0cb8b07019b0a683b22fff92687524f3c9a284106a07a95d9b401338986b1339',
   ];
 
   String currentQuote = '';
@@ -78,7 +83,7 @@ class _FavoritesScreenState extends State<FavoritesScreen>
           'Favorites',
           style: GoogleFonts.poppins(
             textStyle: const TextStyle(
-              fontSize: 28.0,
+              fontSize: 30.0,
               fontWeight: FontWeight.w500,
               color: Colors.white
             ),
@@ -131,6 +136,14 @@ class _FavoritesScreenState extends State<FavoritesScreen>
                         textStyle: const TextStyle(
                           color: Colors.white,
                           fontSize: 30.0,
+                          shadows: <Shadow>
+                          [
+                            Shadow(
+                              offset: Offset(3.0, 3.0),
+                              blurRadius: 10.0,
+                              color: Colors.black,
+                            ),
+                          ],
                         ),
                       ),
                       textAlign: TextAlign.center,
@@ -153,6 +166,14 @@ class _FavoritesScreenState extends State<FavoritesScreen>
                       Icons.share,
                       color: Colors.white,
                       size: 30.0,
+                      shadows: <Shadow>
+                      [
+                        Shadow(
+                          offset: Offset(3.0, 3.0),
+                          blurRadius: 10.0,
+                          color: Colors.black,
+                        ),
+                      ],
                     ),
                   ),
                 ],
